ebe7b4f6e5163331f8f94b406439a252af92b3ac
kuliwebdev <rafif201200@gmail.com>
2025-09-20 12:48:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -360,7 +360,7 @@ yesBtn.addEventListener('click', () => {
     // gunakan ini di script.js (bagian yang mengirim data)
   fetch("https://script.google.com/macros/s/AKfycbxD043F1FOvCWL8JWmqRrRcEzhGHkzV-bA7Iu0Ieu3nTbbh9uWKmq05khv3OzLoZlSnJA/exec", {
   method: "POST",
-  headers: { "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8" },
+  headers: { "Content-Type": "application/x-www-form-urlencoded" },
   body: new URLSearchParams({
     nama: nama,
     alamat: alamat,
@@ -370,19 +370,26 @@ yesBtn.addEventListener('click', () => {
 })
 .then(res => res.text())
 .then(txt => {
+  console.log("Response text:", txt);
   try {
-    const data = JSON.parse(txt || "{}");
-    console.log("Response parsed:", data);
-  } catch (err) {
-    console.warn("Response not JSON or empty:", txt);
+    const data = JSON.parse(txt);
+    console.log("Parsed:", data);
+    alert("Order berhasil dikirim!");
+  } catch (e) {
+    console.warn("Bukan JSON:", txt);
   }
 })
-.catch(err => console.error("Fetch error:", err));
+.catch(err => {
+  console.error("Fetch error:", err);
+  alert("Gagal mengirim data!");
+});
+
 
 
 
 
 });
+
 
 
 
